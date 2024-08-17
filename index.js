84ad97fe546724d6c705a30c3a0d812d222266c9
amoutai <amin.moutai@gmail.com>
2024-08-17 16:15:58 +0200

    adding support for -l

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { readFileSync } = require('fs');
 
+const ARGUMENTS_MAPPING = {
+	c: Buffer.byteLength,
+	l: getLines,
+};
+
 const { argv } = process;
 
 if (argv.length < 3) {
@@ -15,16 +20,23 @@ for (let i = 2; i < argv.length; i++) {
 	}
 }
 
-const fileArgument = argv[argv.length - 1];
-const file = readFileSync(fileArgument);
-
-const ARGUMENTS_MAPPING = {
-	c: Buffer.byteLength,
-};
+const filename = argv[argv.length - 1];
+const file = readFileSync(filename);
 
 let ans = '';
 for (const arg of arguments) {
+	if (!(arg in ARGUMENTS_MAPPING)) {
+		console.log(`ccwc: illegel option -- ${arg}`);
+		continue;
+	}
 	ans += ARGUMENTS_MAPPING[arg](file);
 }
 
-console.log(ans);
+console.log(`\t${ans} ${filename}`);
+
+function getLines(file) {
+	if (!file) {
+		return 0;
+	}
+	return file.toString('utf8').split('\n').length -1
+}
